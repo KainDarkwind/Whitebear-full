bd8d908d9da112d6959c73625594c0afbd221144
kaindarkwind <74018069+KainDarkwind@users.noreply.github.com>
2020-12-02 23:36:55 -0500

    character counters

--- script.js
+++ script.js
@@ -14,9 +14,40 @@ $("#sign-up").click(function () {
 $("#delete-button").click(function () {
    $("#delete-card").toggleClass("d-none");
 });
+let createImageryInputCharsCount = 0;
+$("#create-imagery-input").keypress(function () {
+   createImageryInputCharsCount += 1;
 
-$("#create-imagery-input").click(function () {
-   console.log("you clicked shit");
+   const maxChars = 240;
+   console.log("add it", createImageryInputCharsCount);
+   $("#create-char-count").html(createImageryInputCharsCount);
+});
+
+let createAnswerInputCharsCount = 0;
+$("#create-answer-input").keypress(function () {
+   createAnswerInputCharsCount += 1;
+
+   const maxChars = 240;
+   console.log("add it", createAnswerInputCharsCount);
+   $("#create-answer-char-count").html(createAnswerInputCharsCount);
+});
+
+let editTopInputCharsCount = 0;
+$("#top-text-edit").keypress(function () {
+   editTopInputCharsCount += 1;
+
+   const maxChars = 240;
+   console.log("add it", editTopInputCharsCount);
+   $("#top-char-count").html(editTopInputCharsCount);
+});
+
+let editBottomInputCharsCount = 0;
+$("#bottom-text-edit").keypress(function () {
+   editBottomInputCharsCount += 1;
+
+   const maxChars = 240;
+   console.log("add it", editBottomInputCharsCount);
+   $("#bottom-char-count").html(editBottomInputCharsCount);
 });
 
 // $("#lets-go").click(function () {
