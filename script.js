c66191130eb4f3165d9ad9659b92240a79a393bf
kaindarkwind <74018069+KainDarkwind@users.noreply.github.com>
2020-12-07 10:50:05 -0500

    password and email checks

--- script.js
+++ script.js
@@ -77,32 +77,6 @@ $("#answer-input").keyup(function (e) {
    $("#answer-char-count").html(textLength);
 });
 
-// let createAnswerInputCharsCount = 0;
-// $("#create-answer-input").keypress(function () {
-//    createAnswerInputCharsCount += 1;
-
-//    console.log("add it", createAnswerInputCharsCount);
-//    $("#create-answer-char-count").html(createAnswerInputCharsCount);
-// });
-
-// let editTopInputCharsCount = 0;
-// $("#top-text-edit").keypress(function () {
-//    editTopInputCharsCount += 1;
-
-//    const maxChars = 240;
-//    console.log("add it", editTopInputCharsCount);
-//    $("#top-char-count").html(editTopInputCharsCount);
-// });
-
-let editBottomInputCharsCount = 0;
-$("#bottom-text-edit").keypress(function () {
-   editBottomInputCharsCount += 1;
-
-   const maxChars = 240;
-   console.log("add it", editBottomInputCharsCount);
-   $("#bottom-char-count").html(editBottomInputCharsCount);
-});
-
 let editTopCharsCount = 0;
 $("#top-text-edit").keyup(function (e) {
    console.log("Event", e);
@@ -163,6 +137,104 @@ $("#bottom-text-edit").keyup(function (e) {
 
    //update the character counter on the page
    $("#bottom-char-count").html(textLength);
+});
+
+$("#lets-go").click(function (e) {
+   console.log("Event", e);
+
+   //check if email empty
+
+   const emailInput = $("#sign-up-email").val();
+   const emailLength = emailInput.length;
+   console.log(`email inputted: ${emailInput}`);
+
+   if (emailLength === 0) {
+      console.log("no email entered");
+      //remove d-none from #no-email-warning  add class 'is-invalid' to #sign-up-email
+      $("#no-email-warning").removeClass("d-none");
+      $("#sign-up-email").addClass("is-invalid");
+   } else if (emailLength > 0) {
+      console.log("just right email");
+      $("#no-email-warning").addClass("d-none");
+      $("#sign-up-email").removeClass("is-invalid");
+   }
+
+   //check if password empty
+
+   const passwordInput = $("#sign-up-password").val();
+   const passwordLength = passwordInput.length;
+   console.log(`password inputted: ${passwordInput}`);
+
+   if (passwordLength === 0) {
+      console.log("no password entered");
+      //remove d-none from #no-email-warning  add class 'is-invalid' to #sign-up-email
+      $("#no-password-warning").removeClass("d-none");
+      $("#sign-up-password").addClass("is-invalid");
+   } else if (passwordLength < 9) {
+      console.log("short password entered");
+      $("#short-password-warning").removeClass("d-none");
+      $("#no-password-warning").addClass("d-none");
+      $("#sign-up-password").addClass("is-invalid");
+   } else if (passwordLength > 8) {
+      console.log("just right password");
+      $("#short-password-warning").addClass("d-none");
+      $("#no-password-warning").addClass("d-none");
+      $("#sign-up-password").removeClass("is-invalid");
+   }
+
+   // if (textLength === 0) {
+   //    console.log("no bottom text entered");
+   //    $("#save-edit-card").addClass("disabled");
+   // } else if (textLength > 240) {
+   //    console.log("too much bottom entered");
+   //    $("#save-edit-card").addClass("disabled");
+   //    $("#bottom-char-count").addClass("text-danger");
+   // } else if (textLength > 0 || textLength <= 240) {
+   //    console.log("just right");
+   //    $("#save-edit-card").removeClass("disabled");
+   //    $("#bottom-char-count").removeClass("text-danger");
+   // }
+
+   //check if password long enough
+   // console.log(`The length is: ${textLength}`);
+});
+
+$("#sign-up-email").keyup(function (e) {
+   console.log("Event", e);
+
+   //get the text from textarea
+
+   const text = e.target.value;
+   console.log(`inputted: ${text}`);
+
+   //check the length of the text
+
+   const textLength = text.length;
+   console.log(`The length is: ${textLength}`);
+
+   if (textLength === 0) {
+      console.log("email empty");
+   }
+});
+
+$("#sign-up-password").keyup(function (e) {
+   console.log("Event", e);
+
+   //get the text from textarea
+
+   const text = e.target.value;
+   console.log(`inputted: ${text}`);
+
+   //check the length of the text
+
+   const textLength = text.length;
+   console.log(`The length is: ${textLength}`);
+
+   if (textLength === 0) {
+      console.log("password empty");
+   } else if (textLength < 9) {
+      console.log("password short");
+   }
 });
 
 // $("#lets-go").click(function () {
