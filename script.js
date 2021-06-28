2a5ece919f606634b7413d471c3b7e6fc47655d1
kaindarkwind <74018069+KainDarkwind@users.noreply.github.com>
2021-06-28 17:01:31 -0400

    Initial Char Counters WP4

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ $("#sign-up").click(function () {
 $("#delete-button").click(function () {
    $("#delete-card").toggleClass("d-none");
 });
+
+let createImageryInputCharsCount = 0;
+$("#create-imagery-input").keypress(function () {
+   console.log("pressed key in text area");
+
+   createImageryInputCharsCount += 1; /*This lets me add 1 to the variable 
+   createImageryInputCharsCount++; also would add 1.
+   */
+   console.log("Total input chars: ", createImageryInputCharsCount);
+   $("#create-char-count").html(createImageryInputCharsCount);
+});
+
+let editTopCharsCount = 0;
+$("#edit-top-input").keypress(function () {
+   console.log("pressed key in text area");
+
+   editTopCharsCount += 1; /*This lets me add 1 to the variable 
+   createImageryInputCharsCount++; also would add 1.
+   */
+   console.log("Total input chars: ", editTopCharsCount);
+   $("#edit-top-char-count").html(editTopCharsCount);
+});
+
+let editBottomCharsCount = 0;
+$("#edit-bottom-input").keypress(function () {
+   console.log("pressed key in text area");
+
+   editBottomCharsCount += 1; /*This lets me add 1 to the variable 
+   createImageryInputCharsCount++; also would add 1.
+   */
+   console.log("Total input chars: ", editBottomCharsCount);
+   $("#edit-bottom-char-count").html(editBottomCharsCount);
+});
